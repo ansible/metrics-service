f618ca3c64c3d0fa61034e95d695d249bdf1c59d
Martin Hradil <mhradil@redhat.com>
2026-02-19 17:59:30 +0000

    Fix: Update collectors/__init__.py to import new collectors
    - Remove imports for deleted collectors (collect_host_metrics_hourly, collect_main_host_hourly)
    - Add imports for new collectors (unified_jobs, credentials, execution_environments)
    - Update __all__ exports to match

--- apps/tasks/collectors/__init__.py
+++ apps/tasks/collectors/__init__.py
@@ -1,8 +1,9 @@
 """Collector task functions for the metrics_collectors queue."""
 
-from .collect_host_metrics_hourly import collect_host_metrics_hourly
+from .collect_credentials_hourly import collect_credentials_hourly
+from .collect_execution_environments_daily import collect_execution_environments_daily
 from .collect_job_host_summary_hourly import collect_job_host_summary_hourly
-from .collect_main_host_hourly import collect_main_host_hourly
+from .collect_unified_jobs_hourly import collect_unified_jobs_hourly
 from .daily_anonymize_and_prepare import daily_anonymize_and_prepare
 from .daily_metrics_rollup import daily_metrics_rollup
 from .send_anonymized_to_segment import send_anonymized_to_segment
@@ -10,8 +11,10 @@ from .send_anonymized_to_segment import send_anonymized_to_segment
 __all__ = [
     # Hourly collection tasks (MAP phase)
     "collect_job_host_summary_hourly",
-    "collect_host_metrics_hourly",
-    "collect_main_host_hourly",
+    "collect_unified_jobs_hourly",
+    "collect_credentials_hourly",
+    # Daily collection tasks
+    "collect_execution_environments_daily",
     # Daily rollup and anonymization tasks (REDUCE + ANONYMIZE + SEND)
     "daily_metrics_rollup",
     "daily_anonymize_and_prepare",
