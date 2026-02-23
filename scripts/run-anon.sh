a7d0c30fb63d527b8f65efa85242045f70753817
Martin Hradil <mhradil@redhat.com>
2026-02-23 13:05:51 +0000

    scripts/run-anon.sh - sample script to run all the anonymization tasks

---
+++ scripts/run-anon.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+set -x
+
+uv run scripts/run_task.py collect_hourly_metrics '{"collector_type": "credentials_service"}'
+uv run scripts/run_task.py collect_hourly_metrics '{"collector_type": "job_host_summary_service"}'
+# disabled for now: uv run scripts/run_task.py collect_hourly_metrics '{"collector_type": "main_jobevent_service"}'
+uv run scripts/run_task.py collect_hourly_metrics '{"collector_type": "unified_jobs"}'
+
+# these just once per day, same table (misnomer but works)
+uv run scripts/run_task.py collect_snapshot_metrics '{"collector_type": "config"}'
+uv run scripts/run_task.py collect_snapshot_metrics '{"collector_type": "controller_version_service"}'
+uv run scripts/run_task.py collect_snapshot_metrics '{"collector_type": "execution_environments"}'
+uv run scripts/run_task.py collect_snapshot_metrics '{"collector_type": "table_metadata"}'
+
+# this reads from tasks_hourlymetricscollection and writes to tasks_dailymetricssummary
+uv run scripts/run_task.py daily_metrics_rollup
+
+# this reads from tasks_dailymetricssummary and writes to tasks_anonymizedmetricspayload
+uv run scripts/run_task.py daily_anonymize_and_prepare
+
+# this reads from tasks_anonymizedmetricspayload and sends to segment
+uv run scripts/run_task.py send_anonymized_to_segment
+
+echo SUCCESS
+
+# collect specific hour, for now
+#uv run scripts/run_task.py collect_hourly_metrics '{"collector_type": "unified_jobs", "hour_timestamp": "20250613T10:00:00Z"}'
