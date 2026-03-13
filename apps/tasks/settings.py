225c9137ae02618fb8655e24a831f9cfedf9116d
Ciaran Shiels <cas.ireland@Gmail.com>
2026-03-13 16:09:39 +0000

    Patched needed for deploying in jenkins

--- apps/tasks/settings.py
+++ apps/tasks/settings.py
@@ -5,6 +5,5 @@ DISPATCHERD_ENABLED = True
 Dispatcherd is always enabled in this service (can be disabled in tests)
 Override with METRICS_SERVICE_DISPATCHERD_ENABLED environment variable"""
 
-
-SEGMENT_WRITE_KEY = "test-segment-write-key-change-in-production"
-"""Segment Write Key"""
+# """Segment Write Key this needs to be set in environment variables to run locally in producitoon its mounted as a file"""
+# SEGMENT_WRITE_KEY = "test-segment-write-key-change-in-production"
