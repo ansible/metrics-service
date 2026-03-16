ff2eab1414accc07fdafb455999f5e791b2c3e67
Martin Hradil <mhradil@redhat.com>
2026-03-16 21:10:55 +0000

    run-anon: add the 20250613 timestamp to all the bits, run 24 hourlies

--- scripts/run-anon.sh
+++ scripts/run-anon.sh
@@ -2,26 +2,36 @@
 set -e
 set -x
 
-uv run scripts/run_task.py collect_hourly_metrics '{"collector_type": "credentials_service", "hour_timestamp": "20250613T10:00:00Z"}'
-uv run scripts/run_task.py collect_hourly_metrics '{"collector_type": "job_host_summary_service", "hour_timestamp": "20250613T10:00:00Z"}'
-# disabled for now: uv run scripts/run_task.py collect_hourly_metrics '{"collector_type": "main_jobevent_service", "hour_timestamp": "20250613T10:00:00Z"}'
-uv run scripts/run_task.py collect_hourly_metrics '{"collector_type": "unified_jobs", "hour_timestamp": "20250613T10:00:00Z"}'
+# cleanup
+uv run ./manage.py shell <<EOF
+HourlyMetricsCollection.objects.all().delete()
+DailyMetricsSummary.objects.all().delete()
+AnonymizedMetricsPayload.objects.all().delete()
+EOF
 
-# TODO dates from here on (20250613)
-# these just once per day, same table (misnomer but works)
-uv run scripts/run_task.py collect_snapshot_metrics '{"collector_type": "config"}'
-uv run scripts/run_task.py collect_snapshot_metrics '{"collector_type": "controller_version_service"}'
-uv run scripts/run_task.py collect_snapshot_metrics '{"collector_type": "execution_environments"}'
-uv run scripts/run_task.py collect_snapshot_metrics '{"collector_type": "table_metadata"}'
+# 24 hourly collections, .collection_timestamp = since
+for hour in `seq 0 23`; do
+  uv run scripts/run_task.py collect_hourly_metrics '{"collector_type": "credentials_service", "hour_timestamp": "20250613T'`printf %02d "$hour"`':00:00Z"}'
+  uv run scripts/run_task.py collect_hourly_metrics '{"collector_type": "job_host_summary_service", "hour_timestamp": "20250613T'`printf %02d "$hour"`':00:00Z"}'
+  # disabled for now: uv run scripts/run_task.py collect_hourly_metrics '{"collector_type": "main_jobevent_service", "hour_timestamp": "20250613T'`printf %02d "$hour"`':00:00Z"}'
+  uv run scripts/run_task.py collect_hourly_metrics '{"collector_type": "unified_jobs", "hour_timestamp": "20250613T'`printf %02d "$hour"`':00:00Z"}'
+done
+
+# these just run once per day, same table (misnomer but works)
+# .collection_timestamp = since:23:0:0
+uv run scripts/run_task.py collect_snapshot_metrics '{"collector_type": "config", "collection_timestamp": "20250613T23:00:00Z"}'
+uv run scripts/run_task.py collect_snapshot_metrics '{"collector_type": "controller_version_service", "collection_timestamp": "20250613T23:00:00Z"}'
+uv run scripts/run_task.py collect_snapshot_metrics '{"collector_type": "execution_environments", "collection_timestamp": "20250613T23:00:00Z"}'
+uv run scripts/run_task.py collect_snapshot_metrics '{"collector_type": "table_metadata", "collection_timestamp": "20250613T23:00:00Z"}'
 
 # dump to hourly_dumps/
 uv run scripts/dump_hourly.py
 
-# this reads from tasks_hourlymetricscollection and writes to tasks_dailymetricssummary
-uv run scripts/run_task.py daily_metrics_rollup
+# this reads from tasks_hourlymetricscollection and writes to tasks_dailymetricssummary; doesn't accept datetime, just date
+uv run scripts/run_task.py daily_metrics_rollup '{"summary_date": "20250613"}'
 
-# this reads from tasks_dailymetricssummary and writes to tasks_anonymizedmetricspayload
-uv run scripts/run_task.py daily_anonymize_and_prepare
+# this reads from tasks_dailymetricssummary and writes to tasks_anonymizedmetricspayload; doesn't accept datetime, just date
+uv run scripts/run_task.py daily_anonymize_and_prepare '{"summary_date": "20250613"}'
 
 # dump to daily_dumps/
 uv run scripts/dump_daily_anonymized.py
