ef41c13e3bb38fe0fc69edeb4aec68d9c1ff45b0
MilanPospisil <arkanus@seznam.cz>
2026-06-17 12:53:10 +0200

    Safer timeouts to keep hourly tasks in its boundaries

--- apps/tasks/collectors/fake_hourly_collectors.py
+++ apps/tasks/collectors/fake_hourly_collectors.py
@@ -8,8 +8,8 @@ Each fake task:
 - Then randomly succeeds or fails
 
 Timeout behaviour:
-- TASK_TIMEOUT_SECONDS (6 min): dispatcherd kills the worker if execution exceeds 6 min from start
-- TASK_ABSOLUTE_TIMEOUT_SECONDS (8 min): task is not submitted / not retried if 8 min elapsed since creation
+- TASK_TIMEOUT_SECONDS (4 min): dispatcherd kills the worker if execution exceeds 4 min from start
+- TASK_ABSOLUTE_TIMEOUT_SECONDS (5 min): task is not submitted / not retried if 5 min elapsed since creation
 
 Run `manage.py metrics_service init-system-tasks` after setting TEST_FAKE_TASKS=true
 to create the tasks in the database.
@@ -25,8 +25,8 @@ from apps.tasks.utils import create_task_result
 
 logger = logging.getLogger(__name__)
 
-_TIMEOUT_SECONDS = 60 * 6  # TASK_TIMEOUT_SECONDS: max execution time from started_at
-_ABSOLUTE_TIMEOUT_SECONDS = 60 * 8  # TASK_ABSOLUTE_TIMEOUT_SECONDS: max total time from created
+_TIMEOUT_SECONDS = 60 * 4  # TASK_TIMEOUT_SECONDS: max execution time from started_at
+_ABSOLUTE_TIMEOUT_SECONDS = 60 * 5  # TASK_ABSOLUTE_TIMEOUT_SECONDS: max total time from created
 _MAX_ATTEMPTS = 8
 _RETRY_DELAY_SECONDS = 10
 _RETRY_EXPONENT = 1.5
--- apps/tasks/task_groups.py
+++ apps/tasks/task_groups.py
@@ -182,8 +182,8 @@ METRICS_COLLECTION_GROUP = TaskGroup(
             "cron": "5 * * * *",  # Every hour at XX:05
             "args": {
                 "collector_type": "job_host_summary_service",
-                "TASK_TIMEOUT_SECONDS": 60 * 6,
-                "TASK_ABSOLUTE_TIMEOUT_SECONDS": 60 * 8,
+                "TASK_TIMEOUT_SECONDS": 60 * 4,
+                "TASK_ABSOLUTE_TIMEOUT_SECONDS": 60 * 5,
                 "retry_delay_seconds": 10,
                 "retry_exponent": 1.5,
             },
@@ -197,8 +197,8 @@ METRICS_COLLECTION_GROUP = TaskGroup(
             "cron": "10 * * * *",  # Every hour at XX:10
             "args": {
                 "collector_type": "unified_jobs",
-                "TASK_TIMEOUT_SECONDS": 60 * 6,
-                "TASK_ABSOLUTE_TIMEOUT_SECONDS": 60 * 8,
+                "TASK_TIMEOUT_SECONDS": 60 * 4,
+                "TASK_ABSOLUTE_TIMEOUT_SECONDS": 60 * 5,
                 "retry_delay_seconds": 10,
                 "retry_exponent": 1.5,
             },
@@ -212,8 +212,8 @@ METRICS_COLLECTION_GROUP = TaskGroup(
             "cron": "15 * * * *",  # Every hour at XX:15
             "args": {
                 "collector_type": "credentials_service",
-                "TASK_TIMEOUT_SECONDS": 60 * 6,
-                "TASK_ABSOLUTE_TIMEOUT_SECONDS": 60 * 8,
+                "TASK_TIMEOUT_SECONDS": 60 * 4,
+                "TASK_ABSOLUTE_TIMEOUT_SECONDS": 60 * 5,
                 "retry_delay_seconds": 10,
                 "retry_exponent": 1.5,
             },
@@ -227,8 +227,8 @@ METRICS_COLLECTION_GROUP = TaskGroup(
             "cron": "20 * * * *",  # Every hour at XX:20
             "args": {
                 "collector_type": "main_jobevent_service",
-                "TASK_TIMEOUT_SECONDS": 60 * 6,
-                "TASK_ABSOLUTE_TIMEOUT_SECONDS": 60 * 8,
+                "TASK_TIMEOUT_SECONDS": 60 * 4,
+                "TASK_ABSOLUTE_TIMEOUT_SECONDS": 60 * 5,
                 "retry_delay_seconds": 10,
                 "retry_exponent": 1.5,
             },
