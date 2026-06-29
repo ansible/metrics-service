54d1c433ef534ab42d2f27189ebf512902feded2
Paul Flanagan <pflanaga@redhat.com>
2026-06-29 11:54:55 +0100

    revert(user): revert cached_property on is_platform_auditor

--- apps/core/models/user.py
+++ apps/core/models/user.py
@@ -1,5 +1,3 @@
-from functools import cached_property
-
 from ansible_base.lib.abstract_models.user import AbstractDABUser
 
 
@@ -12,7 +10,7 @@ class User(AbstractDABUser):
 
     encrypted_fields = ["password"]
 
-    @cached_property
+    @property
     def is_platform_auditor(self) -> bool:
         """True if the user holds the Platform Auditor global RBAC role.
 
